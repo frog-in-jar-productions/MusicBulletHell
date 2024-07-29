599a498a48b41eb93ce1fbeee8a22681b016e75f
McGubb <alex@fakefish.com>
2024-07-29 17:44:50 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- player.gd
+++ player.gd
@@ -1,7 +1,28 @@
 extends CharacterBody2D
 
+@export var speed = 400
+var screen_size
 
+func _ready():
+	screen_size = get_viewport_rect().size
 
+func _process(delta):
+	#idk how to do it yet as i havent had a look at the ui stuff but we should eventually use the menu keyboard action layout options that users can remap
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
 
 
